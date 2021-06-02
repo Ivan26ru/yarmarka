ebc3e10c3082902a18a8fec85e2df0ab2a8bfddd
i.pisarenko <i.pisarenko@crmguru.ru>
2021-06-02 11:50:41 +0300

    add new clean theme

---
+++ local/templates/yarmarka2021/description.php
@@ -0,0 +1,7 @@
+<?
+$arTemplate = array (
+    'NAME' => 'Шаблон yarmarka2021',
+    'DESCRIPTION' => 'Шаблон yarmarka2021 для социальной сети',
+    'SORT' => '',
+    'TYPE' => '',
+);
--- local/templates/yarmarka2021/header.php
+++ local/templates/yarmarka2021/header.php
@@ -1,1 +1,21 @@
-<?php if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die(); ?>
+<?php
+if(!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)    die();
+?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title><?$APPLICATION->ShowTitle();?></title>
+    <?$APPLICATION->ShowHead();?>
+    <link rel="stylesheet" href="<?=SITE_TEMPLATE_PATH?>/css/main.css">
+
+
+</head>
+<body>
+<div id="panel">
+    <?$APPLICATION->ShowPanel();?>
+</div>
+<h1>header</h1>
