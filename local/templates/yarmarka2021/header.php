1418ecbfcef7561953ab3bb163c0fd135b9a4c02
i.pisarenko <i.pisarenko@crmguru.ru>
2021-06-02 16:39:21 +0300

    add css to bitrix

--- local/templates/yarmarka2021/header.php
+++ local/templates/yarmarka2021/header.php
@@ -4,11 +4,13 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <title><? $APPLICATION->ShowTitle(); ?></title>
+    <?php use \Bitrix\Main\Page\Asset; ?>
+    <?php Asset::getInstance()->addCss(SITE_TEMPLATE_PATH . "/css/main.css"); ?>
+    <?php Asset::getInstance()->addString('<meta name="viewport" content="width=device-width, initial-scale=1">'); ?>
+
     <? $APPLICATION->ShowHead(); ?>
-    <link rel="stylesheet" href="<?= SITE_TEMPLATE_PATH ?>/css/main.css">
+<!--    <link rel="stylesheet" href="--><?//= SITE_TEMPLATE_PATH ?><!--/css/main.css">-->
 </head>
 <body>
 <div id="panel">
